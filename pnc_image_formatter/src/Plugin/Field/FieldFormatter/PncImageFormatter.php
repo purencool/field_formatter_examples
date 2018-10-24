6461f4adfaee98f2385fe5a0929cd67ab95fcb30
Purencool <purencool@gmail.com>
2018-10-24 15:52:29 +1100

    Image formatter

---
+++ pnc_image_formatter/src/Plugin/Field/FieldFormatter/PncImageFormatter.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Drupal\pnc_image_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatterBase;
+
+/**
+ * Plugin implementation of the 'pnc_image_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "pnc_image_formatter",
+ *   label = @Translation("Pnc image formatter"),
+ *   field_types = {
+ *     "image"
+ *   }
+ * )
+ */
+class PncImageFormatter extends ImageFormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      // Implement default settings.
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return [
+      // Implement settings form.
+    ] + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = $this->viewValue($item);
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+
+    $elements = '';
+    $files = $this->getEntitiesToView($item);
+
+    print_r($files); exit;
+    // Early opt-out if the field is empty.
+    if (empty($files)) {
+      return $elements;
+    }
+/*
+    $url = NULL;
+    $image_link_setting = $this->getSetting('image_link');
+    // Check if the formatter involves a link.
+    if ($image_link_setting == 'content') {
+      $entity = $items->getEntity();
+      if (!$entity->isNew()) {
+        $url = $entity->urlInfo();
+      }
+    }
+    elseif ($image_link_setting == 'file') {
+      $link_file = TRUE;
+    }
+
+    $image_style_setting = $this->getSetting('image_style');
+    $url_type_setting = $this->getSetting('url_type');
+    // Collect cache tags to be added for each item in the field.
+    $cache_tags = array();
+    if (!empty($image_style_setting)) {
+      $image_style = $this->imageStyleStorage->load($image_style_setting);
+      $cache_tags = $image_style->getCacheTags();
+    }
+
+    foreach ($files as $delta => $file) {
+      if (isset($link_file)) {
+        $image_uri = $file->getFileUri();
+        $url = Url::fromUri(file_create_url($image_uri));
+      }
+      $cache_tags = Cache::mergeTags($cache_tags, $file->getCacheTags());
+
+      // Extract field item attributes for the theme function, and unset them
+      // from the $item so that the field template does not re-render them.
+      $item = $file->_referringItem;
+      $item_attributes = $item->_attributes;
+      unset($item->_attributes);
+
+      $elements[$delta] = array(
+        '#theme' => 'image_url_formatter',
+        '#item' => $item,
+        '#item_attributes' => $item_attributes,
+        '#image_style' => $image_style_setting,
+        '#url_type' => $url_type_setting,
+        '#url' => $url,
+        '#cache' => array(
+          'tags' => $cache_tags,
+        ),
+      );
+    }
+
+    return $elements;
+*/
+  }
+
+}
